80b30263001aa6f5335da4c1c40f15736e0ac269
AysuMustafazade <mustafazadays16@gmail.com>
2026-02-19 14:20:30 +0000

    Add header file with prototypes and includes

--- shell.h
+++ shell.h
@@ -1,10 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-void prompt(void);
-char **parse_line(char *line);
-int execute_cmd(char **argv);
-char *_which(char *command);
-char *_getenv(const char *name);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
-#endif
+extern char **environ;
+
+char *find_command(char *command);
+void builtin_env(void);
+int parse_line(char *line, char **argv);
+int exec_command(char **argv, char *cmd_path);
+
+#endif 
