07d7047405d9d8e151c4dadfb81373db397b7275
AysuMustafazade <mustafazadays16@gmail.com>
2026-02-19 14:27:58 +0000

    Add header file with prototypes and includes

--- shell.h
+++ shell.h
@@ -8,10 +8,12 @@
 #include <sys/wait.h>
 
 extern char **environ;
+
 char *find_command(char *command);
 void builtin_env(void);
 int exec_command(char **argv, char *cmd_path);
 int parse_line(char *line, char **argv);
 int handle_builtins(char **argv, int *status, char *line);
+void run_command(char **argv, int *status);
 
 #endif
